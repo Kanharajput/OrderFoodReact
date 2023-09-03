d55b652975d96f41cb79f483cd917efabaa3d20b
Kanha Tomar <kanharajput91@gmail.com>
2023-09-03 21:13:17 +0530

    reducer to handle items and total amount

--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -1,10 +1,32 @@
 import CartContext from './cart-context';
+import { useReducer } from 'react';
+
+// default or initial state
+const defaultState = {
+    items:[], 
+    totalAmount:0
+};
+
+// update the states
+const updateCart = (state, action) => {
+    if(action.type == "ADD"){
+        const updatedItems = state.items.concat(action.item);
+        const updatedAmount = state.totalAmount + action.item.price * action.item.amount;
+        return ({items: updatedItems,totalAmount: updatedAmount});
+    }
+
+    return defaultState;
+}
+
+
 
 const CartProvider = (props) => {
 
-    const addItemToCartHandler = () => {
-        
-    }
+    const [currentCart, dispatchCart] = useReducer(updateCart, defaultState);
+    
+    const addItemToCartHandler = (item) => {
+        dispatchCart({type:"ADD", item: item})
+    }  
     
     const removeItemFromCartHandler = () => {
 
@@ -13,8 +35,8 @@ const CartProvider = (props) => {
     // it's dummy right now
     // it will be stateful later on
     const cartContext = {
-        items:[],
-        totalAmount:0,
+        items: currentCart.items,
+        totalAmount: currentCart.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     }
