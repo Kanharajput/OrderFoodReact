4e4178f28dd59394646cb72f470db68b532453e8
Kanha Tomar <kanharajput91@gmail.com>
2023-09-03 16:00:37 +0530

    cart created

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import style from './Cart.module.css';
+
+function Cart(){
+
+    // dummy item
+    // created a list in jsx
+    // start by creating a list then apply map over it 
+    // then return li from map then cover all inside ul
+    const item_name = <ul className={style['cart-items']}> {
+        [{ id: "1", name: "Dal Bati", amount: "₹120", count:2 }].map((item)=>
+            (<li>{item.name}</li>)
+        )}</ul>
+
+    return (
+        <div>
+            {item_name}
+            <div className={style.total}>
+                <span>Total</span>
+                <span>₹120</span>
+            </div>
+            <div className={style.actions}>
+                <button className={style['button--alt']}>Close</button>
+                <button className={style.button}>Order</button>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
