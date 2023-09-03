dc837ddcf33d7b32d59f86a09757040bc1737598
Kanha Tomar <kanharajput91@gmail.com>
2023-09-03 22:13:13 +0530

    get items count by forwardRef

--- src/components/Meals/Input.js
+++ src/components/Meals/Input.js
@@ -1,12 +1,14 @@
 import style from './Input.module.css';
+import React from 'react';
 
-function Input(props){
+    
+const Input = React.forwardRef((props, ref) => {
     return (
         <div className={style.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input {...props.input} />
+            <input ref={ref} {...props.input} />
         </div>
     );
-}
+});
 
 export default Input;
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,22 +1,33 @@
 import Input from './Input';
 import style from './MealItemForm.module.css';
+import { useRef } from 'react';
+
 
 function MealItemForm(props){
-    return (
-        <div className={style.form}>
-            <Input label="Amount"
-              input={{
-                type:"number",
-                id:props.id,
-                min:"1",
-                max:"5",
-                step:"1",
-                defaultValue:"1"
-              }}
-            />        
-            <button>+Add</button>
-        </div>
-    );
+
+  const inputRef = useRef();
+  
+  const submitHandler = event => {
+    event.preventDefault();
+    console.log(inputRef.current.value);
+  }
+  
+  return (
+    <form className={style.form} onSubmit={submitHandler}>
+        <Input label="Amount"
+          ref ={inputRef}
+          input ={{
+            type:"number",
+            id:props.id,
+            min:"1",
+            max:"5",
+            step:"1",
+            defaultValue:"1"
+          }}
+        />        
+        <button>+Add</button>
+    </form>
+  );
 }
 
 export default MealItemForm;
