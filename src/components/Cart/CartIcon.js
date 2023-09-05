ff91c2bc75855e33d2c521c00a7b2157aebb4d9d
Kanha Tomar <kanharajput91@gmail.com>
2023-09-05 17:20:35 +0530

    remove items from the cart and bump cart icon when item added

--- src/components/Cart/CartIcon.js
+++ src/components/Cart/CartIcon.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import style from './CartIcon.module.css';
 import CartContext from './../../store/cart-context';
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 
 function CartIcon(props){
 
     // get the context
     const cartCtx = useContext(CartContext);
+    const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
 
+    const { items } = cartCtx;
     // find out each item's count order by customer
     // dal bati = 2 , gulab jamun = 2. items.length will return 2 as only two items
     // we want 4 to return as total item's count
     // use reduce builtin method , return after doing calculation over each element
-    const noOfCartItems = cartCtx.items.reduce((prevCalc, currentItem) => prevCalc + currentItem.amount , 0)
+    const noOfCartItems = items.reduce((prevCalc, currentItem) => prevCalc + currentItem.amount , 0)
+
+    const btnClasses = `${style.button} ${isBtnHighlighted ? style.bump : ''}`;
+
+    useEffect(() => {
+        if(items.length === 0){
+            return;
+        }
+        setIsBtnHighlighted(true);
+
+        const timer = setTimeout(()=>{
+            setIsBtnHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
 
     return (
         <React.Fragment>
-            <button className={style.button} onClick={props.onClick}>
+            <button className={btnClasses} onClick={props.onClick}>
                 <i className="fa-solid fa-cart-shopping"></i>
                 <span> Your Cart</span>
                 <span className={style.badge}>{noOfCartItems}</span>
--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -9,11 +9,59 @@ const initialStateValues = {
 
 // update the states
 const updateCart = (state, action) => {
-    if(action.type == "ADD"){
-        const updatedItems = state.items.concat(action.item);
+    
+    if(action.type === "ADD"){
+        
+        let updatedItems;
+
         // amount specify the item's count which is the part of item
         const updatedPrice = state.totalPrice + (action.item.price * action.item.amount);
+
+        // return index if the item exits in items list
+        const existingCartItemId = state.items.findIndex(item => item.id === action.item.id)
+        
+        // if item not exist contains -1
+        if(existingCartItemId !== -1){
+            const existingCartItem = state.items[existingCartItemId];
+            const updatedItem = {...existingCartItem, amount:existingCartItem.amount + action.item.amount};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemId] = updatedItem;
+        }
+        
+        else{
+            updatedItems = state.items.concat(action.item);
+        }
+        
         return ({items: updatedItems,totalPrice: updatedPrice});
+    }
+    
+
+    else if(action.type === "REMOVE"){
+        const RemovingItemIndex = state.items.findIndex(item => item.id === action.id);
+        const ItemToBeRemoved = state.items[RemovingItemIndex];
+        const ItemAmount = ItemToBeRemoved.amount;
+        // decrease the total price by user removed item
+        const updatedPrice = state.totalPrice - ItemToBeRemoved.price;
+        let updatedItems;
+        let updatedItem;
+
+        // if amount is greater than one then minus amount by 1 
+        if(ItemAmount > 1){
+            // initialise the updatedItem with the old value
+            updatedItem = {...ItemToBeRemoved};
+            // decrease the amount by 1
+            updatedItem.amount = ItemAmount - 1;
+            updatedItems = [...state.items];
+            updatedItems[RemovingItemIndex] = updatedItem;
+        }
+        
+        // only single instance item in the list then remove it from the items
+        else{
+            // create new array by excluding the item which has the id equal to action id
+            updatedItems = state.items.filter(item => item.id != action.id);
+        }
+
+        return ({items: updatedItems, totalPrice: updatedPrice});
     }
 
     return initialStateValues;
@@ -26,12 +74,11 @@ const CartProvider = (props) => {
     const [currentCart, dispatchCart] = useReducer(updateCart, initialStateValues);
     
     const addItemToCartHandler = (item) => {
-        dispatchCart({type:"ADD", item: item})
+        dispatchCart({type:"ADD", item: item});
     }  
     
-    const removeItemFromCartHandler = () => {
-        
-
+    const removeItemFromCartHandler = (id) => {
+        dispatchCart({type:"REMOVE", id: id});
     }
 
     // it's dummy right now
